9c12a401f4dfd52a200b2279b514297eecb806ee
Henok Matheas Kebede <henokmatheaskebede@gmail.com>
2022-01-16 10:00:25 +0300

    models.py file finished

--- base/models.py
+++ base/models.py
@@ -1,44 +1,116 @@
+from cv2 import CascadeClassifier
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE,SET_NULL
+from django.db.models.deletion import CASCADE, SET_NULL
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from souq import settings
 
 
-class Shop(models.Model):
-    host = models.ForeignKey(User, on_delete= models.SET_NULL, null = True)
-    name = models.CharField()
-    description = models.TextField(null=True, blank=True)
-    customers = models.ManyToManyField(User,related_name='customers', blank='true')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    location = models.CharField()
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    is_customer = models.BooleanField(default=False)
+    is_seller = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
-    class Meta:
-        ordering = ['-updated','-created']
-
-
-    def __str__(self):
-        return str(self.name)
-
-
-class Material(models.Model):
-    name = models.CharField()
-    detail = models.CharField()
-    price = models.FloatField()
-    quantity = models.IntegerField()
-    # owner = models.ForeignKey()
-    shop = models.ForeignKey(Shop, on_delete= models.CASCADE)
-
-    def __str__(self):
-        return str(self.name)
-
-
-class Rating(models.Model):
-    name = models.CharField()
-    bio = models.CharField()
-    material = models.ForeignKey(Material, on_delete= models.CASCADE)
-    location = models.CharField()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []  # Email & Password are required by default.
 
     def __str__(self):
         return self.name
 
-    # customers =
+    # def has_perm(self, perm, obj=None):
+    #     "Does the user have a specific permission?"
+    #     # Simplest possible answer: Yes, always
+    #     return True
+
+    # def has_module_perms(self, app_label):
+    #     "Does the user have permissions to view the app `app_label`?"
+    #     # Simplest possible answer: Yes, always
+    #     return True
+
+    @property
+    def is_customer(self):
+        "Is the user a member of staff?"
+        return self.is_customer
+
+    @property
+    def is_seller(self):
+        "Is the user a admin member?"
+        return self.is_seller
+
+
+class Shop(models.Model):
+    host = models.ForeignKey(User, models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    customers = models.ManyToManyField(User, related_name="customers", blank="true")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    address = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ["-updated", "-created"]
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    detail = models.CharField(max_length=500)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    # owner = models.ForeignKey()
+    shop = models.ForeignKey(Shop, models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class ItemReview(models.Model):
+    name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=250)
+    rating = models.IntegerField()
+    Item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ShopReview(models.Model):
+    name = models.CharField(max_length=100)
+    comment = models.CharField(max_length=250)
+    rating = models.IntegerField()
+    Item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    image = models.ImageField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_final_price(self):
+        return self.get_total_item_price()
